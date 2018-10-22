808b0ab28b762a92d790be473f9ba06c350cb249
mergitto <merugitosu@gmail.ccom>
2018-10-22 13:21:17 +0900

    定数ファイルのサンプルを作成した

---
+++ const-sample.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def to_float64(vector_list):
+    return np.asarray(vector_list, dtype=np.float64)
+
+# 入力に用いた単語（文章）の分散表現を↓に追加
+SHIKAKU = to_float64([
+        #insert vector
+    ])
+
+
