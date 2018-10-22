810be1cbb19f09ec99c1f713b50996a4d0fb9e77
mergitto <merugitosu@gmail.ccom>
2018-10-22 13:29:49 +0900

    ベクトルはword2vecを読み込むことにする

--- const-sample.py
+++ const-sample.py
@@ -3,9 +3,9 @@ import numpy as np
 def to_float64(vector_list):
     return np.asarray(vector_list, dtype=np.float64)
 
+model = Word2Vec.load("Your word2vec model name")
+
 # 入力に用いた単語（文章）の分散表現を↓に追加
-SHIKAKU = to_float64([
-        #insert vector
-    ])
+SHIKAKU = to_float64(model['input word here'])
 
 
