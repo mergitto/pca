de2451e8b7feda4cca608f624d2e1e69b879d140
mergitto <merugitosu@gmail.ccom>
2018-10-22 13:34:39 +0900

    ロードするファイル名を引数に取るようにした

--- report_to_vector.py
+++ report_to_vector.py
@@ -3,8 +3,8 @@ from collections import defaultdict
 from const import *
 
 
-def load_advice():
-    with open("./advice_classification_2.pickle", 'rb') as f:
+def load_advice(load_file_name):
+    with open(load_file_name, 'rb') as f:
         return pickle.load(f)
 
 def pluck_vector_sum(reports):
@@ -25,7 +25,7 @@ def dump_pickle(data, save_file_name):
         pickle.dump(data, f)
 
 if __name__ == '__main__':
-    reports = load_advice()
+    reports = load_advice("./advice_classification_2.pickle")
 
     reports_vector = pluck_vector_sum(reports)
     add_vector(reports_vector, key='input_word', value=SHIKAKU)
