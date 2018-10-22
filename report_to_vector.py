e88aef3215940be630037194e63ed88f8629b991
mergitto <merugitosu@gmail.ccom>
2018-10-22 13:09:43 +0900

    辞書に値を追加する関数設定

--- report_to_vector.py
+++ report_to_vector.py
@@ -33,6 +33,9 @@ def pluck_vector_sum(reports):
             reports_vector['low'].append(report['vectorSum'])
     return reports_vector
 
+def add_vector(dictionary, key=None, value=None):
+    return dictionary['input_word'].append(SHIKAKU)
+
 def dump_pickle(data, save_file_name):
     with open(save_file_name, 'wb') as f:
         pickle.dump(data, f)
@@ -40,9 +43,9 @@ def dump_pickle(data, save_file_name):
 if __name__ == '__main__':
     reports = load_advice()
 
-    reports_vector = pluck_vector_sum
-    reports_vector['input_word'].append(SHIKAKU)
-    reports_vector['input_word'].append(SHIKAKU)
+    reports_vector = pluck_vector_sum(reports)
+    add_vector(reports_vector, key='input_word', value=SHIKAKU)
+    add_vector(reports_vector, key='input_word', value=SHIKAKU)
 
     dump_pickle(reports_vector, 'report_vector_shikaku.pickle')
 
